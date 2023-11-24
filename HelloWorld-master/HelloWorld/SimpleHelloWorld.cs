6c461ca1afc9bf66cf36ea7ca47fda75979da10d
NikaKhurtsilava <khurtsilava.nika22@gtu.ge>
2023-11-24 17:43:12 +0400

    task1
    task1

---
+++ HelloWorld-master/HelloWorld/SimpleHelloWorld.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    
+    public static class SimpleHelloWorld
+    {
+        
+        public static string ReturnHelloWorld()
+        {
+            // TODO: Return string "Hello, world!";
+            
+            var greeting = "Hello, world!";
+            return greeting;
+        }
+    }
+}
