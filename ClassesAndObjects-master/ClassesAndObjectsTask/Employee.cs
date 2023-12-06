df64f722071f9b494b5bf5961fdda032cf9d7393
NikaKhurtsilava <khurtsilava.nika22@gtu.ge>
2023-12-06 14:42:16 +0400

    Task6
    Task6

---
+++ ClassesAndObjects-master/ClassesAndObjectsTask/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndObjectsTask
+{
+    internal class Employee
+    {
+
+        private string surname { get; set; }
+
+        private int age { get; set; }
+        
+        public Employee() { }
+
+        public Employee(string initialSurname, int initialAge)
+        {
+            surname = initialSurname;
+            age = initialAge;
+        }
+
+        private string GetAge()
+        {
+            return age.ToString();
+        }
+
+        public void SetSurname(string newSurname)
+        {
+            surname = newSurname;
+        }
+
+        public string ReturnEmployeeInfo()
+        {
+            return $"Surname: {surname}, Age: {age}";
+
+        }
+
+
+    }
+}
